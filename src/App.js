27576696044a0fcf0c022d32caf65832f9e24690
AounServices <engineering@aounservices.com>
2020-11-01 12:31:25 +0200

    change2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Welcome to Tuqa App
         </a>
       </header>
     </div>
